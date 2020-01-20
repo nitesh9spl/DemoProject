3dc53e90ed82f77649681fa217319610245bb463
nitesh.jain <nitesh.jain@desktop6>
2020-01-20 14:37:46 +0530

    again

--- DemoProjectTest/src/main/java/Learning/DemoProjectTest/App.java
+++ DemoProjectTest/src/main/java/Learning/DemoProjectTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worlds more!" );
+        System.out.println( "Hello Worlds more! check" );
     }
 }
