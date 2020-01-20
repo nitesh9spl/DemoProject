ac62bdc44e10c02555f47b174ffa9d2923654ffb
nitesh.jain <nitesh.jain@desktop6>
2020-01-20 15:22:55 +0530

    master 1

--- DemoProjectTest/src/main/java/Learning/DemoProjectTest/App.java
+++ DemoProjectTest/src/main/java/Learning/DemoProjectTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worlds mores! check" );
+        System.out.println( "Hello Worlds moresytyt master! check" );
     }
 }
